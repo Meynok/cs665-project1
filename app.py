fdb63ec4abf06ab869e5ec0477f9f35b71e9df71
Meynok <117496073+Meynok@users.noreply.github.com>
2025-11-19 19:50:56 -0600

    Basic app and DB connection setup

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
-import flask
+import sqlite3
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+
+# Database Helper Function
+def get_db_connection():
+    connection = sqlite3.connect('fake_db.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
